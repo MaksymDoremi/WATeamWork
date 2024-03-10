6969975bbece7b0890d876be2748749e28950d91
Dominik Husák <domca.husak@gmail.com>
2024-03-10 11:04:43 +0100

    Order cards update

--- pages/order.php
+++ pages/order.php
@@ -11,21 +11,21 @@
       </div>
   
       <div class="col-xl-4 col-md-6 col-sm-12 shadow-lg" style="border-radius: 8px;">
-          <div class="card" style="background-image: url('/img/Noodles.jpg'); background-size: cover; background-position: center; color: white; border: none; border-radius: 8px;">
+          <div class="card" style="background-image: url('/img/SushiBox.jpg'); background-size: cover; background-position: center; color: white; border: none; border-radius: 8px;">
             <div class="card-body" style="background-color: rgba(0, 0, 0, 0.6); border-radius: 8px;">
-              <h4 class="card-title">Japanese Noodles</h4>
-              <p class="card-text">A noodle dish that is a breath of Asian cuisine and brings a unique taste explosion.</p>
-              <a href="#" class="btn btn-primary">Order for 6$</a>
+              <h4 class="card-title">Sushi Box</h4>
+              <p class="card-text">Order our exclusive Sushi Box containing different types of fresh sushi.</p>
+              <a href="#" class="btn btn-primary">Order for 5$</a>
             </div>
           </div>
       </div>
   
       <div class="col-xl-4 col-md-6 col-sm-12 shadow-lg" style="border-radius: 8px;">
-          <div class="card" style="background-image: url('/img/Noodles.jpg'); background-size: cover; background-position: center; color: white; border: none; border-radius: 8px;">
+          <div class="card" style="background-image: url('/img/NikuUdon.jpg'); background-size: cover; background-position: center; color: white; border: none; border-radius: 8px;">
             <div class="card-body" style="background-color: rgba(0, 0, 0, 0.6); border-radius: 8px;">
               <h4 class="card-title">Japanese Noodles</h4>
-              <p class="card-text">A noodle dish that is a breath of Asian cuisine and brings a unique taste explosion.</p>
-              <a href="#" class="btn btn-primary">Order for 6$</a>
+              <p class="card-text">Savor the flavors of tender beef slices and savory broth in this authentic Japanese dish.</p>
+              <a href="#" class="btn btn-primary">Order for 4$</a>
             </div>
           </div>
       </div>
